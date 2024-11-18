23f45bf7cf85f1c45eecead81ba8f80cb5bf4cb4
okurei <167979336+okurei@users.noreply.github.com>
2024-11-18 14:11:27 +0100

    Create base

---
+++ src/main/java/com/oku/library/jpa/entity/Inventory.java
@@ -0,0 +1,31 @@
+package com.oku.library.jpa.entity;
+
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Inventory {
+    @Id
+    @SequenceGenerator(name = "inventory_id", sequenceName = "inventory_id", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inventory_id")
+    private Long inventoryId;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column
+    private Long stock;
+
+    @OneToOne(targetEntity = Book.class)
+    @JoinColumn(referencedColumnName = "isbn")
+    private Book book;
+}
